37f2ea1074abe14c73feb8e5396262f1d30d9fe9
John Mark Redding <johnmarkredding@gmail.com>
2018-11-14 19:55:35 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = {}
+    data.each do |cat, list|
+      list.each do |k,v|
+        v.each do |x|
+          if new_data[x] == nil
+            new_data[x] = {}
+          end
+          new_data[x][cat] = k
+        end
+      end
+    end
+  new_data
 end
