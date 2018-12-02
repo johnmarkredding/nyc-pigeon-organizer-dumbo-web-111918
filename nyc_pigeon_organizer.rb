d36d8805fe401df21e794ba677da1ab62361f1fb
John Mark Redding <johnmarkredding@gmail.com>
2018-12-02 03:05:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,27 @@
 def nyc_pigeon_organizer(data)
-  new_data = {}
-    data.each do |cat, list|
-      list.each do |k,v|
-        v.each do |x|
-          if new_data[x] == nil
-            new_data[x] = {}
-          end
-          if new_data[x][cat] == nil
-            new_data[x][cat] = [k.to_s]
-          else
-            new_data[x][cat] << k.to_s
-          end
-        end
-      end
-    end
-  new_data
+  new_hash = {}
+  
+  data.
+  
 end
+
+
+
+# def nyc_pigeon_organizer(data)
+#   new_data = {}
+#     data.each do |cat, list|
+#       list.each do |k,v|
+#         v.each do |x|
+#           if new_data[x] == nil
+#             new_data[x] = {}
+#           end
+#           if new_data[x][cat] == nil
+#             new_data[x][cat] = [k.to_s]
+#           else
+#             new_data[x][cat] << k.to_s
+#           end
+#         end
+#       end
+#     end
+#   new_data
+# end
